d21afd1c3772edfb006ae49a2d0f847d63ecd1d3
Sammuel Reis <sammugr@hotmail.com>
2020-04-13 22:21:32 -0300

    implements main page api and styles

--- index.js
+++ index.js
@@ -2,9 +2,9 @@
  * @format
  */
 
-import {AppRegistry} from 'react-native';
+import { AppRegistry } from 'react-native';
 import App from './src';
-import {name as appName} from './app.json';
+import { name as appName } from './app.json';
 
 require('react-native').unstable_enableLogBox();
 
